82d389687afa6ffbfa8fa193957998b9bc1664ad
chityalasrakshin <bsrakshin@gmail.com>
2026-06-29 18:34:34 +0530

    line

---
+++ pipeline.py
@@ -0,0 +1,13 @@
+from agents import build_reader_agent, build_search_agent, writer_chain, critic_chain
+
+def run_research_pipeline(topic : str) -> dir:
+    state = {}
+    # search agent working
+    print("\n" "="*50)
+    print("Step 1 - Search agent is working ...")
+    print("="*50)
+
+    search_agent = build_search_agent()
+    search_agent.invoke({
+        "messages" : [("user", f"Find recent, reliable and detailed information about: {topic}")]
+    })
