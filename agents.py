6e9112790f3ffd9ec8c9a2ca1df06770d08dcf20
chityalasrakshin <bsrakshin@gmail.com>
2026-06-29 23:27:38 +0530

    using gemini

--- agents.py
+++ agents.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search, scrape_url 
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # model setup
-llm = ChatOpenAI(model = "gpt-4o-mini", temperature=0)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
 
 # 1st agent
 def build_search_agent():
